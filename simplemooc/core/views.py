23334d819e3a66cba83db3815174bb387899f98e
Juliana Gonçalves <julianagoncalves.jgp@gmail.com>
2016-02-21 02:33:34 -0300

    Incluindo import UTF-8

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 from .models import Course
