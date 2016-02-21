38de2708d6144832758560d9dae683f669ab8bf4
Juliana Gonçalves <julianagoncalves.jgp@gmail.com>
2016-02-21 02:22:15 -0300

    First commit

---
+++ simplemooc/urls.py
@@ -0,0 +1,30 @@
+"""simplemooc URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
+    url(r'^cursos/', include('simplemooc.courses.urls', namespace='courses')),
+    # referencia ao arquivo urls.py da app Core / namespace nomeia a url de acordo com a app, para evitar conflito com outras apps
+    # no template: {% url 'core:home' %}
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
